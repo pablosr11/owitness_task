f0f4c41c465bbe9316fc5cb63b8155b5c5007b5a
ps <pablosr11@gmail.com>
2021-11-03 11:52:13 +0000

    Add sqlalchemy dependency and boilerplate sqlite DB connection and utils

--- app/main.py
+++ app/main.py
@@ -1,4 +1,25 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# On apis where we expect a large number of concurrent request, we
+# would have to switch to a client/server DB and pool connections.
+# For demo purposes, sqlite.
+DB_URI = "sqlite:///orbital.db"
+engine: Engine = create_engine(
+    url=DB_URI,
+    connect_args={"check_same_thread": False},
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
