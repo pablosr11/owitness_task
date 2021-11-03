8fac767fdcf79abab3679e31f076b0feb2549fe0
ps <pablosr11@gmail.com>
2021-11-03 12:01:06 +0000

    Add DB Models and Pydantic schemas. First endpoint for retrieving single titles.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+from pydantic import BaseModel
+from enum import Enum
 from fastapi import FastAPI
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Text, Integer
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from sqlalchemy.sql.sqltypes import String
 
 # On apis where we expect a large number of concurrent request, we
 # would have to switch to a client/server DB and pool connections.
@@ -21,9 +26,58 @@ def get_db():
     finally:
         db.close()
 
+
+class TitleClass(str, Enum):
+    Freehold = "Freehold"
+    Leasehold = "Leasehold"
+
+
+class SortKeys(str, Enum):
+    id = "id"
+    title_number = "title_number"
+
+
+class OrderKeys(str, Enum):
+    asc = "asc"
+    desc = "desc"
+
+
+class Title(BaseModel):
+    id: int
+    title_number: str
+    title_class: TitleClass
+    content: str
+
+    class Config:
+        orm_mode = True
+
+
+class TitleOutput(BaseModel):
+    id: int
+    title_number: str
+    title_class: TitleClass
+
+    class Config:
+        orm_mode = True
+
+
+class DBTitle(Base):
+    __tablename__ = "titles"
+    id = Column(Integer, primary_key=True)
+    title_number = Column(Text)
+    title_class = Column(String(10))
+    content = Column(Text)
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+async def get_title_by_id(title_id: int, db: Session) -> DBTitle:
+    return db.query(DBTitle).filter(DBTitle.id == title_id).first()
+
+@app.get("/api/titles/{title_id}", response_model=Title)
+async def titles_detail(title_id: int, db: Session = Depends(get_db)):
+    output: DBTitle = await get_title_by_id(title_id=title_id, db=db)
+    if not output:
+        raise HTTPException(status_code=404, detail="Title not found")
+    return output
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,19 @@ from app.main import app
 
 client = TestClient(app)
 
+def test_titles_detail():
+    response = client.get("/api/titles/1")
+    assert response.status_code == 200
+    assert all(
+        key in response.json()
+        for key in ("content", "id", "title_number", "title_class")
+    )
+
+
+def test_title_detail_path_validation():
+    response = client.get("/api/titles/sdf")
+    assert response.status_code == 422
+
 
 def test_list_titles():
     response = client.get("/api/titles/")
